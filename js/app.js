40bd7af5796f6223abd060ba0c43449155896ec6
Abi Vickery <vickeryabi@gmail.com>
2020-01-13 15:14:27 -0600

    testing the limits to calling a function
    called a function inside a function inside a function inside a function inside a function inside a function inside a function inside a function insiad=esl..... you get the idea...

--- js/app.js
+++ js/app.js
@@ -1,193 +1,325 @@
-var foo = 'marvel'; // marvel is not defined
-{
-    var iron = 'Iron Man';
-    let tony = 'Tony Stark';
-    const black = 'Black Widow';
-    console.log(iron);
-    console.log(tony);
-    console.log(black);
+// var foo = 'marvel'; // marvel is not defined
+// {
+//     var iron = 'Iron Man';
+//     let tony = 'Tony Stark';
+//     const black = 'Black Widow';
+//     console.log(iron);
+//     console.log(tony);
+//     console.log(black);
+// }
+// // console.log(marvel); // not defined
+// console.log(tony); // tony is not defined
+
+// var baz = 'dc'; // dc is not defined
+// {
+//     var bat = 'Batman';
+//     let sup = 'Superman';
+//     const lan = 'Green Lantern';
+//     console.log(bat);
+//     console.log(sup);
+//     console.log(lan);
+// }
+// // console.log(dc); // dc is not defined
+// console.log(bat);
+
+// var yo = 'DoctorWho'; // DoctorWho is not defined
+// {
+//     var ten = '10th Doctor';
+//     let elev = '11th Doctor';
+//     const twel = '12th Doctor';
+//     console.log(ten);
+//     console.log(elev);
+//     console.log(twel);
+// }
+// // console.log(DoctorWho); // DoctorWho is not defined
+// console.log(ten);
+
+
+// var fam = 'mlp'; // mlp is not defined
+// {
+//     var loo = 'Luna';
+//     let cel = 'Celestia';
+//     const chry = 'Chrysalis';
+//     console.log(loo);
+//     console.log(cel);
+//     console.log(chry);
+// }
+// // console.log(mlp); // mlp is not defined
+// console.log(loo);
+
+// var pkm = 'Pokemon';
+// {
+//     var pika = 'Pikachu';
+//     let eve = 'Eevee';
+//     const bul = 'Bulbasaur';
+//     console.log(pika);
+//     console.log(eve);
+//     console.log(bul);
+// }
+// // console.log(pkm); // pkm is not defined
+// console.log(pika);
+
+// var morm = 'MoreMarvel';
+// {
+//     var ult = 'Ultron';
+//     let vis = 'Vision';
+//     const thr = 'Thor';
+//     console.log(ult);
+//     console.log(vis);
+//     console.log(thr);
+// }
+// // console.log(morm); // morm is not defined (morm stands for more marvel)
+// console.log(ult);
+
+// function MarvelMCU() {
+//     var hero = [
+//         'Iron Man', 'Captain America', 'Hawkeye', 'Black Widow',
+//         'Hulk', 'Falcon', 'Vision', 'Scarlet Witch', 'War Machine'
+//     ];
+//     var antihero = "Deadpool";
+//     var villain = [
+//         '(fake)Mandarin', 'Ultron', 'Redskull', 'Circus of Crime', 'Taskmaster',
+//         'Abomination', 'Hydra', 'Hyperion', 'Magneto', 'Shocker'
+//     ];
+//     var hero2 = [
+//         'Spider Man', 'Torch', 'Doctor Strange'
+//     ];
+//     var antihero2 = '';
+//     var villain2 = [
+//         'Venom', 'Sandman', 'Rhino', 'Doc ock',
+//         'Vulture', 'Galactus', 'Silver Surfer', 'Dormamu', 'Nightmare'
+//     ];
+//     var aos = [
+//         'Phil Coulson', 'Nick Fury', 'Maria Hill'
+//     ];
+//     var gotg = [
+//         'Star Lord', 'Gamora', 'Rocket Raccoon', 'Groot', 'Drax', 'Mantis', 'Yondu', 'Cosmo'
+//     ];
+//     var gotgvil = [
+//         'Ego', 'Thanos', 'Ronan'
+//     ];
+//     array.forEach(element => {
+//         MediaQueryListEvent(hero);
+//     });
+// }
+// let arr = [
+//     function f1(data) {
+//        var mlp = 'MyLittlePony';
+//        {
+//            var luna = 'Luna';
+//            let cles = 'Celestia';
+//            const chrys = 'Chrysalis';
+//            console.log(luna);
+//            console.log(cles);
+//        } 
+
+//        console.log(mlp);
+//        console.log(luna);
+//     },
+// ];
+
+
+// let arr1 = [
+//     1 = 'Troll',
+//     CustomElementRegistry() = [
+//         2 = 'olo',
+//         menubar() = [
+//             3 = 'lol'
+//         ]
+//     ]
+// ]
+// console.log(arr1)
+
+
+
+
+// for (let a = 0; a < Array.length; a++) {
+//     for (let b = 0; b < Array.length; b++) {
+//         for (let c = 0; c < Array.length; c++) {
+//             for (let d = 0; d < Array.length; d++) {
+//                 for (let e = 0; e < Array.length; e++) {
+//                     for (let f = 0; f < Array.length; f++) {
+//                         for (let g = 0; g < Array.length; g++) {
+//                             for (let h = 0; h < Array.length; h++) {
+//                                 for (let i = 0; i < Array.length; i++) {
+//                                     for (let j = 0; j < Array.length; j++) {
+//                                         for (let k = 0; k < Array.length; k++) {
+//                                             for (let l = 0; l < Array.length; l++) {
+//                                                 for (let m = 0; m < Array.length; m++) {
+//                                                     for (let n = 0; n < Array.length; n++) {
+//                                                         for (let o = 0; o < Array.length; o++) {
+//                                                             for (let p = 0; p < Array.length; p++) {
+//                                                                 for (let q = 0; q < Array.length; q++) {
+//                                                                     for (let r = 0; r < Array.length; r++) {
+//                                                                         for (let s = 0; s < Array.length; s++) {
+//                                                                             for (let t = 0; t < Array.length; t++) {
+//                                                                                 for (let u = 0; u < Array.length; u++) {
+//                                                                                     for (let v = 0; v < Array.length; v++) {
+//                                                                                         for (let w = 0; w < Array.length; w++) {
+//                                                                                             for (let x = 0; x < Array.length; x++) {
+//                                                                                                 for (let y = 0; y < Array.length; y++) {
+//                                                                                                     for (let z = 0; z < Array.length; z++) {
+//                                                                                                         // do stuff
+//                                                                                                     }
+//                                                                                                 }
+//                                                                                             }
+//                                                                                         }
+//                                                                                     }
+//                                                                                 }
+//                                                                             }
+//                                                                         }
+//                                                                     }
+//                                                                 }
+//                                                             }
+//                                                         }
+//                                                     }
+//                                                 }
+//                                             }
+//                                         }
+//                                     }
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// function callback() {
+//     requestAnimationFrame;
+//     Float64Array;
+// }
+
+function z1() {
+    console.log('z.');
+    // do stuff
 }
-// console.log(marvel); // not defined
-console.log(tony); // tony is not defined
 
-var baz = 'dc'; // dc is not defined
-{
-    var bat = 'Batman';
-    let sup = 'Superman';
-    const lan = 'Green Lantern';
-    console.log(bat);
-    console.log(sup);
-    console.log(lan);
-}
-// console.log(dc); // dc is not defined
-console.log(bat);
-
-var yo = 'DoctorWho'; // DoctorWho is not defined
-{
-    var ten = '10th Doctor';
-    let elev = '11th Doctor';
-    const twel = '12th Doctor';
-    console.log(ten);
-    console.log(elev);
-    console.log(twel);
-}
-// console.log(DoctorWho); // DoctorWho is not defined
-console.log(ten);
-
-
-var fam = 'mlp'; // mlp is not defined
-{
-    var loo = 'Luna';
-    let cel = 'Celestia';
-    const chry = 'Chrysalis';
-    console.log(loo);
-    console.log(cel);
-    console.log(chry);
-}
-// console.log(mlp); // mlp is not defined
-console.log(loo);
-
-var pkm = 'Pokemon';
-{
-    var pika = 'Pikachu';
-    let eve = 'Eevee';
-    const bul = 'Bulbasaur';
-    console.log(pika);
-    console.log(eve);
-    console.log(bul);
-}
-// console.log(pkm); // pkm is not defined
-console.log(pika);
-
-var morm = 'MoreMarvel';
-{
-    var ult = 'Ultron';
-    let vis = 'Vision';
-    const thr = 'Thor';
-    console.log(ult);
-    console.log(vis);
-    console.log(thr);
-}
-// console.log(morm); // morm is not defined (morm stands for more marvel)
-console.log(ult);
-
-function MarvelMCU() {
-    var hero = [
-        'Iron Man', 'Captain America', 'Hawkeye', 'Black Widow',
-        'Hulk', 'Falcon', 'Vision', 'Scarlet Witch', 'War Machine'
-    ];
-    var antihero = "Deadpool";
-    var villain = [
-        '(fake)Mandarin', 'Ultron', 'Redskull', 'Circus of Crime', 'Taskmaster',
-        'Abomination', 'Hydra', 'Hyperion', 'Magneto', 'Shocker'
-    ];
-    var hero2 = [
-        'Spider Man', 'Torch', 'Doctor Strange'
-    ];
-    var antihero2 = '';
-    var villain2 = [
-        'Venom', 'Sandman', 'Rhino', 'Doc ock',
-        'Vulture', 'Galactus', 'Silver Surfer', 'Dormamu', 'Nightmare'
-    ];
-    var aos = [
-        'Phil Coulson', 'Nick Fury', 'Maria Hill'
-    ];
-    var gotg = [
-        'Star Lord', 'Gamora', 'Rocket Raccoon', 'Groot', 'Drax', 'Mantis', 'Yondu', 'Cosmo'
-    ];
-    var gotgvil = [
-        'Ego', 'Thanos', 'Ronan'
-    ];
-    array.forEach(element => {
-        MediaQueryListEvent(hero);
-    });
-}
-let arr = [
-    function f1(data) {
-       var mlp = 'MyLittlePony';
-       {
-           var luna = 'Luna';
-           let cles = 'Celestia';
-           const chrys = 'Chrysalis';
-           console.log(luna);
-           console.log(cles);
-       } 
-
-       console.log(mlp);
-       console.log(luna);
-    },
-];
-
-
-let arr1 = [
-    1 = 'Troll',
-    CustomElementRegistry() = [
-        2 = 'olo',
-        menubar() = [
-            3 = 'lol'
-        ]
-    ]
-]
-console.log(arr1)
-
-
-
-
-for (let a = 0; a < Array.length; a++) {
-    for (let b = 0; b < Array.length; b++) {
-        for (let c = 0; c < Array.length; c++) {
-            for (let d = 0; d < Array.length; d++) {
-                for (let e = 0; e < Array.length; e++) {
-                    for (let f = 0; f < Array.length; f++) {
-                        for (let g = 0; g < Array.length; g++) {
-                            for (let h = 0; h < Array.length; h++) {
-                                for (let i = 0; i < Array.length; i++) {
-                                    for (let j = 0; j < Array.length; j++) {
-                                        for (let k = 0; k < Array.length; k++) {
-                                            for (let l = 0; l < Array.length; l++) {
-                                                for (let m = 0; m < Array.length; m++) {
-                                                    for (let n = 0; n < Array.length; n++) {
-                                                        for (let o = 0; o < Array.length; o++) {
-                                                            for (let p = 0; p < Array.length; p++) {
-                                                                for (let q = 0; q < Array.length; q++) {
-                                                                    for (let r = 0; r < Array.length; r++) {
-                                                                        for (let s = 0; s < Array.length; s++) {
-                                                                            for (let t = 0; t < Array.length; t++) {
-                                                                                for (let u = 0; u < Array.length; u++) {
-                                                                                    for (let v = 0; v < Array.length; v++) {
-                                                                                        for (let w = 0; w < Array.length; w++) {
-                                                                                            for (let x = 0; x < Array.length; x++) {
-                                                                                                for (let y = 0; y < Array.length; y++) {
-                                                                                                    for (let z = 0; z < Array.length; z++) {
-                                                                                                        // do stuff
-                                                                                                    }
-                                                                                                }
-                                                                                            }
-                                                                                        }
-                                                                                    }
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+function y1() {
+    console.log('y.');
+    z1();
 }
 
-function callback() {
-    requestAnimationFrame;
-    Float64Array;
+function x1() {
+    console.log('x.');
+    y1();
 }
+
+function w1() {
+    console.log('w.');
+    x1();
+}
+
+function v1() {
+    console.log('v.');
+    w1();
+}
+
+function u1() {
+    console.log('u.');
+    v1();
+}
+
+function t1() {
+    console.log('t.');
+    u1();
+}
+
+function s1() {
+    console.log('s.');
+    t1();
+}
+
+function r1() {
+    console.log('r.');
+    s1();
+}
+
+function q1() {
+    console.log('q.');
+    r1();
+}
+
+function p1() {
+    console.log('p.');
+    q1();
+}
+
+function o1() {
+    console.log('o.');
+    p1();
+}
+
+function n1() {
+    console.log('n.');
+    o1();
+}
+
+function m1() {
+    console.log('m.');
+    n1();
+}
+
+function l1() {
+    console.log('l.');
+    m1();
+}
+
+function k1() {
+    console.log('k.');
+    l1();
+}
+
+function j1() {
+    console.log('j.');
+    k1();
+}
+
+function i1() {
+    console.log('i.');
+    j1();
+}
+
+function h1() {
+    console.log('h.');
+    i1();
+}
+
+function g1() {
+    console.log('g.');
+    h1();
+}
+
+function f1() {
+    console.log('f.');
+    g1();
+}
+
+function e1() {
+    console.log('e.');
+    f1();
+}
+
+function d1() {
+    console.log('d.');
+    e1();
+}
+
+function c1() {
+    console.log('c.');
+    d1();
+}
+
+function b1() {
+    console.log('b.');
+    c1();
+}
+
+function a1() {
+    console.log('a.');
+    b1();
+}
+
+a1();
